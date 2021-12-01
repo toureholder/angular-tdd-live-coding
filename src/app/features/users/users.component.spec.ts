baf42df41f90c9d237cafad59d9928cd88a73e66
Touré Holder <toureholder@gmail.com>
2021-12-01 10:52:52 -0300

    tdd user component ng oninit

--- src/app/features/users/users.component.spec.ts
+++ src/app/features/users/users.component.spec.ts
@@ -1,16 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { User, UserService } from 'src/app/services/user.service';
 
 import { UsersComponent } from './users.component';
 
 describe('UsersComponent', () => {
   let component: UsersComponent;
   let fixture: ComponentFixture<UsersComponent>;
+  let mockUserService: jasmine.SpyObj<UserService>;
 
   beforeEach(async () => {
+    mockUserService = jasmine.createSpyObj('mockUserService', ['fetchUsers']);
+
     await TestBed.configureTestingModule({
-      declarations: [ UsersComponent ]
-    })
-    .compileComponents();
+      declarations: [UsersComponent],
+      providers: [{ provide: UserService, useValue: mockUserService }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +26,35 @@ describe('UsersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#ngOnInit', () => {
+    it('should load users', () => {
+      // Given - User service returrns list of users observable
+      const users: User[] = [
+        {
+          id: 1,
+          email: 'george.bluth@reqres.in',
+          firstName: 'George',
+          lastName: 'Bluth',
+          avatarUrl: 'https://reqres.in/img/faces/1-image.jpg',
+        },
+        {
+          id: 2,
+          email: 'janet.weaver@reqres.in',
+          firstName: 'Janet',
+          lastName: 'Weaver',
+          avatarUrl: 'https://reqres.in/img/faces/2-image.jpg',
+        },
+      ];
+
+      mockUserService.fetchUsers.and.returnValue(of(users));
+
+      // When
+      component.ngOnInit();
+
+      // Then - component users to equal user service list of users
+      expect(component.users).toEqual(users);
+    });
   });
 });
--- src/app/features/users/users.component.ts
+++ src/app/features/users/users.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { User, UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  users: User[] = [];
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.userService.fetchUsers()?.subscribe((val: User[]) => {
+      this.users = val;
+    });
   }
-
 }
